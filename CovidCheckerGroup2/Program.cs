691bcc3926d876786cdd23e9797dc5d83390c994
Nico De Witte <nico.dewitte@vives.be>
2020-10-27 07:48:34 +0100

    Add code

---
+++ CovidCheckerGroup2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace CovidCheckerGroup2
+{
+    class Program
+    {
+
+        public static double RequestUserTemperature()
+        {
+            Console.Write("Please enter your current temperature: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static bool RequestHasSymptoms()
+        {
+            Console.Write("Do you have any symptoms [yes/no]");
+            string userAnswer = Console.ReadLine();
+
+            // if (userAnswer == "yes") {
+            //   return true;
+            // }
+            // else {
+            //   return false;
+            // }
+
+            return (userAnswer == "yes");
+        }
+
+        public static void AssessTemperature(double temperature, bool hasSymptoms)
+        {
+            // if (temperature >= 38) {
+            //   Console.WriteLine("You may have covid. You have a fever.");
+            // }
+            // else {
+            //   if (temperature >= 36.7) {
+            //     Console.WriteLine("You might be sick");
+            //   }
+            //   else {
+            //     Console.WriteLine("All is fine");
+            //   }
+            // }
+
+            // EQUIVALENT:
+            // Order is important !!!
+            if (hasSymptoms && (temperature >= 38))
+            {
+                Console.WriteLine("You have covid. You have a fever.");
+            }
+            else if (temperature >= 38)
+            {
+                Console.WriteLine("Please see a doctor. You have a fever.");
+            }
+            else if (temperature >= 36.7)
+            {
+                Console.WriteLine("You might be sick");
+            }
+            else
+            {
+                Console.WriteLine("All is fine");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to covid checker");
+
+            double temperature = RequestUserTemperature();
+            bool hasSymptoms = RequestHasSymptoms();
+
+            AssessTemperature(temperature, hasSymptoms);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - Decision making
+
+Checking if a user has possible covid using if-else construct
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/CovidCheckerGroup2.git
+```
